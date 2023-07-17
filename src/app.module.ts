c3a6480fe46fe512c806a9c0068f1a659c41d0e7
Sri Ganesh <sriganeshiiits@gmail.com>
2023-07-17 10:12:15 +0530

    register prisma and employees module with the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EmployeesModule } from './employees/employees.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports:[EmployeesModule,PrismaModule],
   controllers: [],
   providers: [],
 })
